59004a4b9af7f3ae811cfd00d989c761c49503cb
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-11 18:47:39 -0400

    add array, add first 2 functions, pass first 3 tests

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
